214925cbbd8315fb7fc5c58d37ebf210b53887db
RamazanSokuoglu <16050111017@ybu.edu.tr>
2021-06-18 23:10:29 +0300

    ...

--- BusinessLayer/Concrete/HesapServices/ShippingCalculator.cs
+++ BusinessLayer/Concrete/HesapServices/ShippingCalculator.cs
@@ -2,9 +2,13 @@
 using DataEntity;
 using DataEntity.DTO;
 using DataEntity.Enums;
+using StudentInfo.BusinessLayer.WebRequester;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete.HesapServices
 {
@@ -12,27 +16,103 @@ namespace BusinessLayer.Concrete.HesapServices
     {
         private readonly List<WeightAndSize> _weightAndSizeList;
 
-        public ShippingCalculator(IWeightAndSizeServices weightAndSizeServices )
+        public ShippingCalculator(IWeightAndSizeServices weightAndSizeServices)
         {
 
             _weightAndSizeList = weightAndSizeServices.GetAll();
         }
 
-        public RsEstimateShipmentDto CalculateShip(string SenderAddress,string ReceiverAddress, int Weight, ShipSize Size)
+        public RsEstimateShipmentDto CalculateShip(string senderAddress, string receiverAddress, int Weight, ShipSize Size)
         {
-            var distance = 100;
-            var tablokarsiligi = _weightAndSizeList.Find(x => x.WeightMin <= Weight && x.WeightMax >= Weight && x.Size==Size).Price;
-            var cash = tablokarsiligi * Weight * distance;
+            var sender = GetCoordinatesForAddress(senderAddress);
+            var receiver = GetCoordinatesForAddress(receiverAddress);
+
+            var distance = CalculateDistance(sender, receiver);
+            var weightSize = _weightAndSizeList.Find(x => x.WeightMin <= Weight && x.WeightMax >= Weight && x.Size == Size);
+
+            var tablokarsiligi = weightSize != null ? weightSize.Price : 0;
+            double cash = Convert.ToDouble(tablokarsiligi) * Weight * distance;
             var result = new RsEstimateShipmentDto
             {
-                Price = cash,
+                Price = Convert.ToDecimal(cash),
                 Weight = Weight,
-                ReceiverAddress = ReceiverAddress,
-                SenderAddress = SenderAddress,
+                ReceiverAddress = senderAddress,
+                SenderAddress = receiverAddress,
                 Size = Size
             };
             return result;
 
         }
+
+        private GeoLocationInfo GetCoordinatesForAddress(string address)
+        {
+            try
+            {
+
+                var url = string.Format("/search.php?q={0}&polygon_geojson=0&format=jsonv2", address);
+
+                var webRequester = new WebRequestManager("https://nominatim.openstreetmap.org");
+                var result = webRequester.DoRequest<List<GeoLocationInfo>>(url, false);
+                if (result != null && result.Count > 0)
+                {
+                    return result.OrderByDescending(o => o.importance).FirstOrDefault();
+
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+            //JsonObject jsonObject = JsonObject.Parse(await httpResult.Content.ReadAsStringAsync());
+
+            //return jsonObject.GetNamedObject("address").GetNamedString("lat"); // <-- what about "lon"?
+            return null;
+        }
+
+        public double CalculateDistance(GeoLocationInfo start, GeoLocationInfo target, char unit = 'K')
+        {
+            if (start != null && target != null)
+            {
+                return CalculateDistance(Convert.ToDouble(start.Lat), Convert.ToDouble(start.Lon), Convert.ToDouble(target.Lat), Convert.ToDouble(target.Lon), unit);
+            }
+            return 0;
+        }
+
+        public double CalculateDistance(double lat1, double lon1, double lat2, double lon2, char unit = 'K')
+        {
+            double rlat1 = Math.PI * lat1 / 180;
+            double rlat2 = Math.PI * lat2 / 180;
+            double theta = lon1 - lon2;
+            double rtheta = Math.PI * theta / 180;
+            double dist =
+                Math.Sin(rlat1) * Math.Sin(rlat2) + Math.Cos(rlat1) *
+                Math.Cos(rlat2) * Math.Cos(rtheta);
+            dist = Math.Acos(dist);
+            dist = dist * 180 / Math.PI;
+            dist = dist * 60 * 1.1515;
+
+            switch (unit)
+            {
+                case 'K': //Kilometers -> default
+                    return dist * 1.609344;
+                case 'N': //Nautical Miles 
+                    return dist * 0.8684;
+                case 'M': //Miles
+                    return dist;
+            }
+
+            return dist;
+        }
+    }
+
+    public class GeoLocationInfo
+    {
+        public decimal Lat { get; set; }
+        public decimal Lon { get; set; }
+        public string display_name { get; set; }
+        public string icon { get; set; }
+        public decimal importance { get; set; }
     }
 }
